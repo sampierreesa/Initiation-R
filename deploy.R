9ab91dfce87a7b437f52aff75233e8731af1896c
sampierreesa <samuel.pierre36@outlook.fr>
2021-09-10 15:30:27 +0200

    shiny app test

---
+++ deploy.R
@@ -0,0 +1,5 @@
+library(rsconnect)
+setAccountInfo(name=Sys.getenv("shinyapps_name"),
+                token=Sys.getenv("shinyapps_token"),
+                secret=Sys.getenv("shinyapps_secret"))
+deployApp()
